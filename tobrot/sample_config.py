8c7e6092a1edb19ad5e4cfb8036e056de6365062
amansharma999 <aman.gamestar999@gmail.com>
2020-07-25 09:55:29 +0530

    Update sample_config.py

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -3,14 +3,14 @@ from tobrot.get_cfg import get_config
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = get_config("1207847913:AAFY2uRLoTAl247ghFDBpLVU-K51pUNudZQ", "")
     # The Telegram API things
-    APP_ID = int(get_config("APP_ID", 12345))
-    API_HASH = get_config("API_HASH")
+    APP_ID = int(get_config("APP_ID", 1108982))
+    API_HASH = get_config("babad37bef120d5c76a1fa02e3c2b5f5")
     # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
     AUTH_CHANNEL = set(
-        int(x) for x in get_config("AUTH_CHANNEL", "").split()
+        int(x) for x in get_config(-1001408486121", "").split()
     )
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "./DOWNLOADS")
